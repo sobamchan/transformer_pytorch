5f44e001086b4f6d9e9574f384f92fd294b1bfa1
sobamchan <oh.sore.sore.soutarou@gmail.com>
2018-06-23 22:55:16 +0900

    [WIP] added train

---
+++ train.py
@@ -0,0 +1,182 @@
+import time
+
+import numpy as np
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+from models import subsequent_mask, make_model
+
+
+class Batch:
+
+    def __init__(self, src, tgt=None, pad=0):
+        self.src = src
+        self.src_mask = (src != pad).unsqueeze(-1)
+        if tgt is not None:
+            self.tgt = tgt[:, :-1]
+            self.tgt_y = tgt[:, 1:]
+            self.tgt_mask = self.make_std_mask(self.tgt, pad)
+            self.token_n = (self.tgt_y != pad).data.sum()
+
+    @staticmethod
+    def make_std_mask(tgt, pad):
+        tgt_mask = (tgt != pad).unsqueeze(-1)
+        tgt_mask = tgt_mask &\
+            Variable(subsequent_mask(tgt.size(-1)).type_as(tgt_mask.data))
+        return tgt_mask
+
+
+def run_epoch(data_iter, model, loss_compute):
+    start = time.time()
+    total_tokens = 0
+    total_loss = 0
+    tokens = 0
+    for i, batch in enumerate(data_iter):
+        out = model.forward(batch.src,
+                            batch.tgt,
+                            batch.src_mask,
+                            batch.tgt_mask)
+        loss = loss_compute(out, batch.tgt_y, batch.token_n)
+        total_loss += loss
+        total_tokens += batch.token_n
+        tokens += batch.token_n
+        if i % 50 == 1:
+            elapsed = time.time() - start
+            print('Epoch Step: %d Loss: %f Tokens per sSec: %f' %
+                  (i, loss / batch.token_n, tokens / elapsed))
+            start = time.time()
+            tokens = 0
+    return total_loss / total_tokens
+
+
+global max_src_in_batch, max_tgt_in_batch
+
+
+def batch_size_fn(new, count, sofar):
+    global max_src_in_batch, max_tgt_in_batch
+    if count == 1:
+        max_src_in_batch = 0
+        max_tgt_in_batch = 0
+    max_src_in_batch = max(max_src_in_batch, len(new.src))
+    max_tgt_in_batch = max(max_tgt_in_batch, len(new.tgt) + 2)
+    src_elements = count * max_src_in_batch
+    tgt_elements = count * max_tgt_in_batch
+    return max(src_elements, tgt_elements)
+
+
+class NoamOpt:
+
+    def __init__(self, model_size, factor, warmup, optimizer):
+        self.optimizer = optimizer
+        self._step = 0
+        self.warmup = warmup
+        self.factor = factor
+        self.model_size = model_size
+        self._rate = 0
+
+    def setup(self):
+        self._step += 1
+        rate = self.rate()
+        for p in self.optimizer.param_groups:
+            p['lr'] = rate
+        self._rate = rate
+        self.optimizer.step()
+
+    def rate(self, step=None):
+        if step is None:
+            step = self._step
+        return self.factor *\
+            (self.model_size ** (-0.5) * min(step ** (-0.5),
+                                             step * self.warmup ** (-1.5)))
+
+
+def get_std_opt(model):
+    return NoamOpt(model.src_embed[0].d_model, 2, 4000,
+                   torch.optim.Adam(model.parameters(),
+                                    lr=0, betas=(0.9, 0.98), eps=1e-9))
+
+
+class LabelSmoothing(nn.Module):
+
+    def __init__(self, size, padding_idx, smoothing=0.0):
+        super(LabelSmoothing, self).__init__()
+        self.criterion = nn.KLDivLoss(size_average=False)
+        self.padding_idx = padding_idx
+        self.confidence = 1.0 - smoothing
+        self.smoothing = smoothing
+        self.size = size
+        self.true_dist = None
+
+    def forward(self, x, target):
+        assert x.size(1) == self.size
+        true_dist = x.data.clone()
+        true_dist.fill_(self.smoothing / (self.size - 2))
+        true_dist.scatter_(1, target.data.unsqueeze(1), self.confidence)
+        true_dist[:, self.padding_idx] = 0
+        mask = torch.nonzero(target.data == self.padding_idx)
+        if mask.dim() > 0:
+            true_dist.index_fill_(0, mask.squeeze(), 0.0)
+            self.true_dist = true_dist
+        return self.criterion(x, Variable(true_dist, requires_grad=False))
+
+
+# opts = [
+#         NoamOpt(512, 1, 4000, None),
+#         NoamOpt(512, 1, 4000, None),
+#         NoamOpt(512, 1, 4000, None),
+#         ]
+# crit = LabelSmoothing(5, 0, 0.1)
+# predict = torch.FloatTensor([[0, 0.2, 0.7, 0.1, 0],
+#                             [0, 0.2, 0.7, 0.1, 0],
+#                             [0, 0.2, 0.7, 0.1, 0]])
+# v = crit(Variable(predict.log()),
+#          Variable(torch.LongTensor([2, 1, 0])))
+
+
+def data_gen(V, batch, batch_n):
+
+    for i in range(batch_n):
+        data = torch.from_numpy(np.random.randint(1, V, size=(batch, 10)))
+        data[:, 0] = 1
+        src = Variable(data, requires_grad=False)
+        tgt = Variable(data, requires_grad=False)
+        yield Batch(src, tgt, 0)
+
+
+class SimpleLossCompute:
+
+    def __init__(self, generator, criterion, opt=None):
+        self.generator = generator
+        self.criterion = criterion
+        self.opt = opt
+
+    def __call__(self, x, y, norm):
+        x = self.generator(x)
+        loss = self.criterion(x.contiguous().view(-1, x.size(-1)),
+                              y.contiguous().view(-1)) / norm
+        loss.backward()
+        if self.opt is not None:
+            self.opt.step()
+            self.opt.optimizer.zero_grad()
+        return loss.data[0] * norm
+
+
+V = 11
+criterion = LabelSmoothing(size=V, padding_idx=0, smoothing=0.0)
+model = make_model(V, V, n=2)
+model_opt = NoamOpt(model.src_embed[0].d_model, 1, 400,
+                    torch.optim.Adam(model.parameters(),
+                                     lr=0,
+                                     betas=(0.9, 0.98),
+                                     eps=1e-9))
+
+
+for epoch in range(10):
+    model.train()
+    run_epoch(data_gen(V, 30, 20), model,
+              SimpleLossCompute(model.generator, criterion, model_opt))
+    model.eval()
+    print(run_epoch(data_gen(V, 30, 5), model,
+                    SimpleLossCompute(model.generator, criterion, None)))
