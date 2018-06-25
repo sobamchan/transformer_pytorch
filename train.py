899e3d1360529d5be782c008284a64b691348582
sobamchan <oh.sore.sore.soutarou@gmail.com>
2018-06-25 21:41:45 +0900

    added decoding for toy data

--- train.py
+++ train.py
@@ -180,3 +180,26 @@ for epoch in range(10):
     model.eval()
     print(run_epoch(data_gen(V, 30, 5), model,
                     SimpleLossCompute(model.generator, criterion, None)))
+
+
+def greedy_decode(model, src, src_mask, max_len, start_symbol):
+    memory = model.encode(src, src_mask)
+    ys = torch.ones(1, 1).fill_(start_symbol).type_as(src.data)
+    for i in range(max_len - 1):
+        out = model.decode(memory, src_mask, Variable(ys),
+                           Variable(subsequent_mask(ys.size(1))
+                                    .type_as(src.data)))
+        prob = model.generator(out[:, -1])
+        _, next_word = torch.max(prob, dim=1)
+        next_word = next_word.data[0]
+        ys = torch.cat([
+            ys,
+            torch.ones(1, 1).type_as(src.data).fill_(next_word)
+            ], dim=1)
+    return ys
+
+
+model.eval()
+src = Variable(torch.LongTensor([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]))
+src_mask = Variable(torch.ones(1, 1, 10))
+print(greedy_decode(model, src, src_mask, max_len=10, start_symbol=1))
